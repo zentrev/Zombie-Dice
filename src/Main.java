71eae6457240773281e47787f8796205b95fd9c5
Matthew Nichols <matthewnichols9505@gmail.com>
2017-10-27 11:39:27 -0600

    hi

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
 
         System.out.println("This is a git test");
-        System.out.println("sean sucks");
+        System.out.println("sean sucks even more");
     }
 }
 
