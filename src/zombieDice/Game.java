93971974acaf0f2b6f45d0e1649d8403c54a8ca2
Levi Salazar <BSalazar@student.neumont.edu>
2017-10-30 11:14:03 -0600

    test

--- src/zombieDice/Game.java
+++ src/zombieDice/Game.java
@@ -1,16 +1,42 @@
 package zombieDice;
 
-import Frame.PlayerPanel;
-import java.lang.String;
+import java.util.Scanner;
 
 public class Game {
+    public void startGame() {
+        Scanner scanner = new Scanner(System.in);
 
-    public void startGame(){
+
+
         //makes players
         //goes threw every players rounds
         //once a player gets 13 brains run one round for every other player
         //end the game showing the winner
 
-    }
 
+// for some reasons the two scanners are not the same
+
+        boolean p = true;
+        boolean n = true;
+        String playerx = "";
+        while (p) {
+            while (n) {
+                System.out.println("Please enter the name of the Player: ");
+                playerx = scanner.nextLine();
+                if (!playerx.equals("") && playerx.length() > 0) {
+                    n = false;
+                }
+            }
+
+            Players.add(new Players(playerx));
+            System.out.println("Is there another player?: ");
+            String input = scanner.nextLine();
+
+            if (!input.equalsIgnoreCase("yes")) {
+                p = false;
+            }
+        }
+
+    }
 }
+
--- src/zombieDice/Players.java
+++ src/zombieDice/Players.java
@@ -1,9 +1,14 @@
 package zombieDice;
 
 public class Players {
+
     private String playerName = "";
     private int brains = 0;
 
+    public Players(String playerSet){
+        this.playerName = playerSet;
+        this.brains = 0;
+    }
     //makes object of players
     public String players(){
         return playerName;
