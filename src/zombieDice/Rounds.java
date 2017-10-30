5db70ab5dbae81c81a08670dbe6eb1ef95e986c6
C.T.Raines.III <c.t.raines.iii@gmail.com>
2017-10-30 11:01:24 -0600

    Clifford

--- src/zombieDice/Rounds.java
+++ src/zombieDice/Rounds.java
@@ -1,23 +1,23 @@
 package zombieDice;
 
 public class Rounds {
-    //Should set shots and brains to 0 and dice != runner (see below)
-    //check if strikes are lower then 3 if not end round
-    //run turns until round is over
-    //stores brains until round is over
-    //give option to end round and collect current brains
+        //Should set shots and brains to 0 and dice != runner (see below)
+        //check if strikes are lower then 3 if not end round
+        //run turns until round is over
+        //stores brains until round is over
+        //give option to end round and collect current brains
 
-    //we need to set the dice array in turns to shots or brains, This way we don't carry the runners from the last round
+        //we need to set the dice array in turns to shots or brains, This way we don't carry the runners from the last round
+    public void Round(){
+
+        Placeholder dice = new Placeholder();
+        Turns turn = new Turns();
+        int shots = turn.returnShots();
+        while (shots < 30) {
+
+        }
 
 
-    Placeholder dice = new Placeholder();
-    Turns turn = new Turns();
-    int shots = turn.returnShots();
-    while(shots < 3){
-
+        Placeholder2 players = new Placeholder2();
     }
-
-
-    Placeholder2 players = new Placeholder2();
-
 }
