9c0dcd0b69c46e69df64c8bb95b5e71caabc4bd0
C.T.Raines.III <c.t.raines.iii@gmail.com>
2017-10-31 11:33:04 -0600

    Clifford-

--- src/zombieDice/Turns.java
+++ src/zombieDice/Turns.java
@@ -19,26 +19,28 @@ public class Turns {
 
         //checks if we have a runner
         for (int i = 0; i < 3; i++) {
-            if (!(die[i].getStatus()).equals("runner")) {
+            if (!(die[i].getStatus()).equals("Runner")) {
                 die[i].changeColor();
             }
-        }
 
-        //rolls dice based on color
-
-
-
+            die[i].rollDice();
+            if((die[i].getStatus()).equals("Brains")){
+                brains++;
+            }
+            if((die[i].getStatus()).equals("SHOT!")){
+                shots++;
+            }
             System.out.println("\u001b[33m" + "the color is " + die[i].getColor() + ", the state is " + die[i].getStatus());
 
-            //die[i].display(); or something, maybe i will put in a gui and do it there
         }
+
+
     }
 
     //return statements
     public static int returnBrains() {
         return brains;
     }
-
     public static int returnShots() {
         return shots;
     }
