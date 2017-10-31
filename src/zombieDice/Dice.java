2ce17e3d61fdfc3b10218630f81ae7406da9ad0b
Kyle Sunday <kyle.sunday@yahoo.com>
2017-10-31 10:56:55 -0600

    hello

--- src/zombieDice/Dice.java
+++ src/zombieDice/Dice.java
@@ -4,20 +4,42 @@ public class Dice {
     //make dice object that can be red yellow or green
     //holds status of die, brain-runner-shot
     //has ability not to change dice in case it lands on runner
-    
 
-    private String color;
-    private String status;
 
-    public void die(){
+    private String color = null;
+    private String status = "";
+
+    public Dice() {
         this.color = "";
         this.status = "";
+
     }
 
-    public String getColor(){return color;}
-    public String getStatus(){return status;}
-    public void setStatus(String stat){
-        this.status = stat;
+    public String getColor() {
+        return color;
+
+
+    }
+
+    public String getStatus() {
+        return status;
+
+
+    }
+
+    public void changeColor() {
+        int Rando = Math.random() * 100;
+        if (Rando <= 46) {
+            this.color = "green";
+
+
+        }
+        else if (Rando > 46 && Rando <= 77){
+            this.color = "yellow";
+        }
+        else (Rando > 77){
+            this.color = "RED";
+        }
     }
 
 }
