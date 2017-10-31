b2c022a9f567def1f457ef8099bb1c320f328829
Kyle Sunday <kyle.sunday@yahoo.com>
2017-10-31 11:26:41 -0600

    hello

--- src/zombieDice/Dice.java
+++ src/zombieDice/Dice.java
@@ -40,5 +40,59 @@ public class Dice {
             this.color = "RED";
         }
     }
+    public void rollDice(){
+        int Mando = (int) (math.random() * 6);
+        if (this.getColor().equals("green") ) {
 
+            switch (Mando) {
+                case 0:
+                case 1:
+                case 2:
+                    this.status = "Brains";
+                    break;
+                case 3:
+                case 4:
+                    this.status = "Runner";
+                    break;
+                case 5:
+                    this.status = "SHOT!";
+                    break;
+            }
+        }
+        else if (this.getColor().equals("yellow")){
+            switch (Mando){
+                case 0:
+                case 1:
+                    this.status = "Brains";
+                    break;
+                case 2:
+                case 3:
+                    this.status = "Runner";
+                    break;
+                case 4:
+                case 5:
+                    this.status = "SHOT!";
+                    break;
+            }
+        }
+        else if (this.getColor().equals("RED")){
+            switch (Mando){
+                case 0:
+                    this.status = "Brains";
+                    break;
+                case 1:
+                case 2:
+                    this.status = "Runner";
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    this.status = "SHOT!";
+                    break;
+            }
+        }
+
+
+
+    }
 }
